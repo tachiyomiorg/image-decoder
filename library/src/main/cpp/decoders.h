a65850031ecd2b0342985b857b0211753e50316a
inorichi <len@kanade.eu>
2021-05-31 01:09:52 +0200

    Add missing files

---
+++ library/src/main/cpp/decoders.h
@@ -0,0 +1,22 @@
+//
+// Created by len on 30/5/21.
+//
+
+#ifndef IMAGEDECODER_DECODERS_H
+#define IMAGEDECODER_DECODERS_H
+
+#include "decoder_headers.h"
+#ifdef HAVE_LIBJPEG
+#include "decoder_jpeg.h"
+#endif
+#ifdef HAVE_LIBPNG
+#include "decoder_png.h"
+#endif
+#ifdef HAVE_LIBWEBP
+#include "decoder_webp.h"
+#endif
+#ifdef HAVE_LIBHEIF
+#include "decoder_heif.h"
+#endif
+
+#endif //IMAGEDECODER_DECODERS_H
---
+++ library/src/main/cpp/libheif/libde265.cmake
@@ -0,0 +1,21 @@
+include(FetchContent)
+
+FetchContent_Declare(libde265
+  GIT_REPOSITORY  https://github.com/strukturag/libde265.git
+  GIT_TAG         v1.0.8
+)
+
+option(BUILD_SHARED_LIBS "" OFF)
+option(ENABLE_SDL "" OFF)
+if (${ANDROID} AND NOT ${ANDROID_ABI} STREQUAL "x86" AND NOT ${ANDROID_ABI} STREQUAL "x86_64")
+  option(DISABLE_SSE "" ON)
+endif()
+
+FetchContent_MakeAvailable(libde265)
+
+set(LIBDE265_FOUND 1)
+set(LIBDE265_LIBRARIES libde265)
+set(LIBDE265_INCLUDE_DIR
+  "${libde265_SOURCE_DIR}"
+  "${libde265_BINARY_DIR}"
+)
